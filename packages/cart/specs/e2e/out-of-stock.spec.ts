00a31327f741c407ac1bbf347d6995b3144e8f9d
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-03-01 11:46:52 +0100

    test: add case for availability error message for multiple items

--- packages/cart/specs/e2e/out-of-stock.spec.ts
+++ packages/cart/specs/e2e/out-of-stock.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "../fixtures/tokenizedPage"
+import { expect, test } from "../fixtures/tokenizedPage"
 
 test.describe("Quantity not available", () => {
   test.use({
@@ -22,5 +22,46 @@ test.describe("Quantity not available", () => {
     await expect(
       CartPage.page.locator("text=The selected quantity is not available")
     ).toBeVisible()
+  })
+})
+
+test.describe("Not available error message should appear only for single line item", () => {
+  test.use({
+    options: {
+      orderType: "with-items",
+      lineItemsAttributes: [
+        {
+          sku_code: "SHIRT34SFFFFFF000000SXXX",
+          quantity: 1,
+        },
+        {
+          sku_code: "BEACHAIRFFFFFF000000XXXX",
+          quantity: 1,
+        },
+      ],
+    },
+  })
+
+  test("should see an error when selecting non available quantity", async ({
+    CartPage,
+  }) => {
+    const itemShirt = CartPage.page.locator("[data-test-id=line-item-skus]", {
+      hasText: "Shirt",
+    })
+    const itemBeanChair = CartPage.page.locator(
+      "[data-test-id=line-item-skus]",
+      {
+        hasText: "Bean Chair",
+      }
+    )
+    await itemShirt.locator("[data-test-id=input-spinner-element]").fill("800")
+
+    await expect(
+      itemShirt.locator("text=The selected quantity is not available")
+    ).toBeVisible()
+
+    await expect(
+      itemBeanChair.locator("text=The selected quantity is not available")
+    ).not.toBeVisible()
   })
 })
